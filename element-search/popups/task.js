0bf4dc1e3cb07bccc2851498660343d55196efe8
Blackcoder070 <blackcoder070@gmail.com>
2023-05-31 16:23:12 +0300

    first commit popups

---
+++ element-search/popups/task.js
@@ -0,0 +1,30 @@
+const mainModalEl = document.querySelector("#modal_main");
+const successModalEl = document.querySelector("#modal_success");
+
+const closeBtnsEls = document.querySelectorAll(".modal__close");
+const goodBtnsEls = document.querySelectorAll(".btn");
+
+const toDoGoodBtnEl = document.querySelector(".show-success");
+
+mainModalEl.classList.add("modal_active");
+
+const allBtnsArr = [...closeBtnsEls, ...goodBtnsEls];
+
+allBtnsArr.forEach((el) => {
+  el.addEventListener("click", function (evt) {
+    const targetEl = evt.target;
+    if (allBtnsArr.includes(targetEl)) {
+      this.closest(".modal").classList.remove("modal_active");
+    }
+  });
+});
+
+toDoGoodBtnEl.addEventListener("click", () => {
+  successModalEl.classList.add("modal_active");
+});
+
+document.addEventListener("keydown", (evt) => {
+  if (evt.key === "Escape") {
+    document.querySelector(".modal_active").classList.remove("modal_active");
+  }
+});
