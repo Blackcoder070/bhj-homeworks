d62f78fbba2443ca177f084b96b59891b5c9e38e
Blackcoder070 <blackcoder070@gmail.com>
2023-05-31 15:28:50 +0300

    first commit cookie-clicker

---
+++ js-features/cookie-clicker/task.js
@@ -0,0 +1,20 @@
+const image = document.getElementById("cookie");
+const counter = document.getElementById("clicker__counter");
+image.onclick = afterClick; 
+
+function count() {
+	counter.textContent++;
+}
+function changeSizes() {
+	if(counter.textContent % 2 === 0){
+		image.width = 300;
+	}
+	else {
+		image.width = 200;
+	}
+};
+
+function afterClick(){
+	count();
+	changeSizes();
+}
